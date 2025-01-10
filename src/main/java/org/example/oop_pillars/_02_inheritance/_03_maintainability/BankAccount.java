688991397a196788f249a798fb4c77fdc6b194c8
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2025-01-10 22:25:53 +0300

    Maintainability setup

---
+++ src/main/java/org/example/oop_pillars/_02_inheritance/_03_maintainability/BankAccount.java
@@ -0,0 +1,64 @@
+package org.example.oop_pillars._02_inheritance._03_maintainability;
+
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+    private String encryptedPassword;
+
+    public BankAccount(String accountNumber, String password, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.encryptedPassword = encryptPassword(password);
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance(String enteredPassword) {
+        if (authenticate(enteredPassword)) {
+            return balance;
+        } else {
+            System.out.println("Invalid password! Access denied.");
+            return -1;
+        }
+    }
+
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Invalid deposit amount!");
+        }
+    }
+
+    public void withdraw(double amount, String enteredPassword) {
+        if (authenticate(enteredPassword)) {
+            if (amount > 0 && amount <= balance) {
+                balance -= amount;
+                System.out.println("Withdrawn: " + amount);
+                logTransaction("Withdraw", amount); // New feature
+            } else {
+                System.out.println("Invalid withdrawal amount or insufficient balance!");
+            }
+        } else {
+            System.out.println("Invalid password! Transaction denied.");
+        }
+    }
+
+    private String encryptPassword(String password) {
+        // Simple encryption logic (placeholder)
+        return "encrypted_" + password;
+    }
+
+    boolean authenticate(String enteredPassword) {
+        return encryptedPassword.equals(encryptPassword(enteredPassword));
+    }
+
+    // New logging method
+    void logTransaction(String type, double amount) {
+        System.out.println(type + " of $" + amount + " logged.");
+    }
+
+}
---
+++ src/main/java/org/example/oop_pillars/_02_inheritance/_03_maintainability/ExtensibilityRunner.java
@@ -0,0 +1,21 @@
+package org.example.oop_pillars._02_inheritance._03_maintainability;
+
+public class ExtensibilityRunner {
+    public static void main(String[] args) {
+        System.out.println("Saving account transactions:");
+        SavingsAccount savingAccount = new SavingsAccount("00900675420", "password", 45000, 20);
+        double initialBalance = savingAccount.getBalance("password");
+        System.out.println("Initial Balance: " + initialBalance);
+        savingAccount.applyInterest("password");
+        double interestAppliedBalance = savingAccount.getBalance("password");
+        System.out.println("Post Interest Balance: " + interestAppliedBalance);
+
+
+        System.out.println("Checking account transactions:");
+        CheckingAccount checkingAccount = new CheckingAccount("200908765002", "abc", 55000, 5000);
+        checkingAccount.withdraw(3000, "abc");
+        checkingAccount.withdraw(53000, "abc");
+        checkingAccount.withdraw(90000, "abc");
+
+    }
+}
